fd245129992340b5f2572fc49987d70236ff63e2
Kavya Negi <negikavya09@gmail.com>
2026-08-13 18:24:56 +0530

    add function

---
+++ function/multiply.py
@@ -0,0 +1,11 @@
+
+"""
+     Multiply of number
+"""
+def sum(a,b):
+    c=a*b
+    print(f"Multiply is {c}")
+
+a=int(input("Enter no. is "))
+b=int(input("Enter no. is "))
+c=sum(a,b)
---
+++ string/reverse.py
@@ -0,0 +1,10 @@
+#unary minus
+
+
+str="I love Python"
+i=1
+l=len(str)
+while i<=l:
+    print(str[-i],end= " ")
+    i+=1
+    print(i)
