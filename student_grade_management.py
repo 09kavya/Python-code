959bf28697bdcac5e67c4763e69da33916ac10db
Kavya Negi <negikavya09@gmail.com>
2026-08-16 21:04:29 +0530

    add student grade management

---
+++ student_grade_management.py
@@ -0,0 +1,23 @@
+"""
+    Student Grade Management System
+    1. Add Student
+    2. Update Student
+    3. Delete Student
+    4. View Student
+    5. Exit
+
+"""
+def management() : 
+    student=[]
+    operation=int(input("Student Grade Management System\n 1. Add Student\n 2. Update Student\n 3. Delete Student\n 4. View Student\n 5. Exit\n Enter your choice :  "))
+    
+    if operation == 1:
+        name=input("Enter student name = ")
+        grade=int(input("Enter student grade = "))
+        student.append(f"{name} : {grade}")
+        print(f"Added {name} with a {grade}")
+
+    
+
+
+management()
