e3fdc54984f0cc24e881e40f5dddd4b434140704
Kavya Negi <negikavya09@gmail.com>
2026-08-21 13:32:14 +0530

    add power question with while loop

--- while loop practice_que/q8.py
+++ while loop practice_que/q8.py
@@ -1,4 +1,11 @@
 # calculate power of a number
 
-num=int(input("Enter the number = "))
-power=int(input("Enter the power = "))  
+num=int(input("Enter the base = "))
+power=int(input("Enter the power = "))
+i=1 
+total=1 
+
+while i<=power :
+    total=num**i
+    i+=1
+print(total)
